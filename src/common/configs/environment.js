ab36d9e79f464723e61273909d2dbd646f3ac5c8
QuocDL <quoclcph18659@gmail.com>
2025-11-01 12:28:10 +0700

    init

---
+++ src/common/configs/environment.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import z from "zod";
+
+dotenv.config({});
+
+const envVarsSchema = z.object({
+  NODE_ENV: z.string(),
+  PORT: z.coerce.number().default(8000),
+  HOST: z.string().min(1).default("127.0.0.1"),
+  DB_URI: z.string().min(1).describe("Local Mongo DB"),
+  JWT_ACCESS_SECRET: z.string(),
+  JWT_ACCESS_EXPIRED: z.string().default("30d"),
+});
+const result = envVarsSchema.safeParse(process.env);
+if (!result.success) {
+  result.error.issues.forEach((issue) => {
+    console.error(`❌ Field "${issue.path.join(".")}" - ${issue.message}`);
+  });
+  console.error(
+    "⛔ Stopping application due to missing environment variables.",
+  );
+  process.exit(1);
+}
+const envVars = result.data;
+
+export const {
+  NODE_ENV,
+  PORT,
+  HOST,
+  DB_URI,
+  JWT_ACCESS_SECRET,
+  JWT_ACCESS_EXPIRED,
+} = envVars;
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import authRoute from "./modules/auth/auth.routes.js";
+
+const routes = Router();
+
+export default routes;
