1d106e1b3e1dc0af79731dfe4fe1f053466a86fe
QuocDL <quoclcph18659@gmail.com>
2025-11-04 16:38:11 +0700

    KHANH: genre service

---
+++ src/modules/genre/genre.controller.js
@@ -0,0 +1,40 @@
+import handleAsync from "../../common/utils/async-handler.js";
+import createResponse from "../../common/utils/create-response.js";
+import {
+  createGenreService,
+  getAllGenreService,
+  getDetailGenreService,
+  updateGenreService,
+  updateStatusGenreService,
+} from "./genre.service.js";
+
+export const getAllGenre = handleAsync(async (req, res) => {
+  const { query } = req;
+  const data = await getAllGenreService(query);
+  return createResponse(res, 200, "OK", data.data, data.meta);
+});
+
+export const getDetailGenre = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  const data = await getDetailGenreService(id);
+  return createResponse(res, 200, "OK", data);
+});
+
+export const createGenre = handleAsync(async (req, res) => {
+  const { body } = req;
+  const data = await createGenreService(body);
+  return createResponse(res, 201, "Tạo thể loại thành công", data);
+});
+
+export const updateGenre = handleAsync(async (req, res) => {
+  const { body, params } = req;
+  const { id } = params;
+  const data = await updateGenreService(id, body);
+  return createResponse(res, 200, "Cập nhật thể loại thành công", data);
+});
+
+export const updateStatusGenre = handleAsync(async (req, res) => {
+  const { id } = req.params;
+  const data = await updateStatusGenreService(id);
+  return createResponse(res, 200, data.message, data.data);
+});
